4f2c7beb09119825545f87f4a1e51a5ea848c507
vishnulakshmi231123 <vishnulakshmi231123@gmail.com>
2025-07-27 09:52:15 +0400

    Add files via upload

---
+++ Ques2.java
@@ -0,0 +1,19 @@
+package question;
+import java.util.Scanner;
+public class Ques2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Enter the number");
+			int x = scan.nextInt();
+			
+			if(x > 0) {
+				System.out.println("positive");
+			} else {
+				System.out.println("Negative");
+			}
+			
+	}
+
+}
